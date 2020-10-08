677ccca2d374a332e518b5c2386883b3a20aa1f5
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2020-10-08 13:21:42 -0600

    Added function to clear form field on button click

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function newItem() {
+    // add new items to list
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -9,18 +10,27 @@ function newItem() {
         $('#list').append(li);
     };
 
+    // enabling item crossout with double click
     li.on("dblclick", function crossOut() {
         li.toggleClass("strike");
     });
 
+    // create delete list item button
     let deleteButton = $('<deleteButton></deleteButton>');
     deleteButton.append(document.createTextNode('X'));
     li.append(deleteButton);
 
+    // adding delete class to list item so item can be hidden with css
     deleteButton.on("click", deleteListItem);
     function deleteListItem() {
         li.addClass('delete');
     };
 
+    // enable items to be reordered by dragging
     $('#list').sortable();
+};
+
+// clears form field after add button is clicked
+function clearField() {
+    document.getElementById('input').value = "";
 };
